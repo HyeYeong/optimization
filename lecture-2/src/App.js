8ba7a4e6cb4fa52dec56b1be35258cee47892fb3
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-06-13 11:12:09 +0900

    feat: 이미지 사전 로드 기법

--- lecture-2/src/App.js
+++ lecture-2/src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header'
 import InfoTable from './components/InfoTable'
 import SurveyChart from './components/SurveyChart'
 import Footer from './components/Footer'
-// import ImageModal from handleMouseEnter
 const LazyImageModal = lazy(() => import('./components/ImageModal'))
 
 function App() {
@@ -12,6 +11,11 @@ function App() {
 
   useEffect(() => {
     const component = import('./components/ImageModal')
+    // NOTE: 첫 번째 사진을 사전로드 해서 모달을 열었을 때의 동작이 지연되지 않도록 해 준다. 다른 사진은 불필요한 리소스로 인해 로드가 늦어질 수 있으니 첫 번째 사진만 사전로드 처리 해 준다. 
+    // 一番目の画像をページへ飛ばす瞬間ロードして、モーダルを開いた時の動作が遅延されないように処理する。他の画像はリソースのダウンロードが多くなるので、１番目のの画像のみにする。
+    const img = new Image();
+    const firstImgSrc = 'https://stillmed.olympic.org/media/Photos/2016/08/20/part-1/20-08-2016-Football-Men-01.jpg?interpolation=lanczos-none&resize=*:800'
+    img.src = `'{${firstImgSrc}}'`
   }, [])
 
   return (
